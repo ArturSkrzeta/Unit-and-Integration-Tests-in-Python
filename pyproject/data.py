476f92ee91b2ce76c52037c9fc0213ee0793aa05
ArturSkrzeta <65757653+ArturSkrzeta@users.noreply.github.com>
2020-11-08 15:02:40 +0100

    Add files via upload

---
+++ pyproject/data.py
@@ -0,0 +1,38 @@
+posts_1 = [
+    {
+        'title': 'Post1',
+        'content': 'Content Post1'
+    },
+    {
+        'title': 'Post2',
+        'content': 'Content Post2'
+    },
+    {
+        'title': 'Post3',
+        'content': 'Content Post3'
+    }
+]
+
+posts_2 = [
+    {
+        'title': 'Post1',
+        'content': 'Content Post1'
+    },
+    {
+        'title': 'Post2',
+        'content': 'Content Post2'
+    }
+]
+
+blogs = [
+    {
+        'title': 'Blog1',
+        'author': 'Artur',
+        'posts': posts_1
+    },
+    {
+        'title': 'Blog2',
+        'author': 'Mike',
+        'posts': posts_2
+    }
+]
